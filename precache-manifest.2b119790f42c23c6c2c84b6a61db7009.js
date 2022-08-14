85e4771d63778c3548039974a575cd0d62af0a8a
Om Patil <200010036@iitdh.ac.in>
2022-08-14 22:44:42 +0530

    Updates

---
+++ precache-manifest.2b119790f42c23c6c2c84b6a61db7009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39b68111caf471a1430565b8a41be45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293c6329ade74d5794c5",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "ad7a89901a493b72ba4c",
+    "url": "/static/js/2.0a738854.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0a738854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293c6329ade74d5794c5",
+    "url": "/static/js/main.a4772407.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "4f7fca79f59b18655f1848860bb6b7f8",
+    "url": "/static/media/resume.4f7fca79.pdf"
+  }
+]);
